118499aa8c01282fd8674f93b93c64971412ba70
RJ <jensenrs@gmail.com>
2017-10-31 07:45:14 -0700

    all main routes functional

---
+++ src/models/task-models.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const tasks = JSON.parse(fs.readFileSync('./db/tasks.json', 'utf-8'))
+const uuid = require('uuid-v4')
+
+function getAllTasks(){
+  return tasks
+}
+
+function getOneTask(id){
+  const thisTask = tasks.find(el => el.id === id)
+
+  let response
+  if (!thisTask) {
+    response = {errors: {message: "could not find that task"}}
+  } else {
+    response = thisTask
+  }
+  return response
+}
+
+function createTask(body){
+  let response
+  if (!body) {
+    response = {errors: {message: "please enter a task"}}
+  } else {
+    tasks.push(body)
+    const result = JSON.stringify(tasks)
+    fs.writeFileSync('./db/tasks.json', result, 'utf-8')
+    response = body
+  }
+  return response
+}
+
+function editTask(id, body){
+  const thisTask = tasks.find(el => el.id === id)
+
+  let response
+  if (!thisTask) {
+    response = {errors: {message: "could not find that task"}}
+  } else if (!body.task) {
+    response = {errors: {message: "please enter a task"}}
+  } else {
+    thisTask.task = body.task
+    const result = JSON.stringify(tasks)
+    fs.writeFileSync('./db/tasks.json', result, 'utf-8')
+    response = thisTask
+  }
+  return response
+}
+
+function destroyTask(id){
+  const thisTask = tasks.find(el => el.id === id)
+
+  let response
+  if (!thisTask) {
+    response = {errors: {message: "could not find that task"}}
+  } else {
+    const index = tasks.indexOf(thisTask)
+    const deletedTask = tasks.splice(index, 1)
+    const result = JSON.stringify(tasks)
+    fs.writeFileSync('./db/tasks.json', result, 'utf-8')
+    response = deletedTask
+  }
+  return response
+}
+
+
+module.exports = { getAllTasks, getOneTask, createTask, editTask, destroyTask }
